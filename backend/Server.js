5523452ace6b4e24c5dd1c4d4cc996eb89bc6668
Arsalan Ahmad <coderpatlu@gmail.com>
2025-12-04 00:27:05 +0500

    KINTO IS NOW ON GIT HUB

---
+++ backend/Server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from  'dotenv';
+
+import { connectDB } from './db.js';
+
+connectDB();
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/users", (req, res) => {
+    res.json({
+        message : "Users Api WOrking"
+    });
+
+})
+
+app.listen(5000, () =>console.log("Server Running on Port 5000"));
